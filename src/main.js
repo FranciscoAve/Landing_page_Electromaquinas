8a8a0d7f144340cd56419004b176e7f2ef09d577
Francisco Avendaño <favendan@espol.edu.ec>
2025-11-03 07:32:33 +0000

    improving js use

--- src/main.js
+++ src/main.js
@@ -3,22 +3,17 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const btnCatalogo = document.getElementById("btnCatalogo");
+//const footer = document.createElement("div"); aqui podemos agregar el footer dinamicamente espero
+//const contenido = document.createElement('div'); // contenedor dinámico
+//document.body.appendChild(contenido);
 
-setupCounter(document.querySelector('#counter'))
+
+
+
+btnCatalogo.addEventListener("click", () => {
+  window.open("/pages/product_catalog.html", "_self");
+});
+
+
+
